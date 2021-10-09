dd399da990d960751d35c3ebe2afefefef770c1c
Simon Adcock <simonadcock2@gmail.com>
2021-10-09 09:21:15 +0100

    add direction listener

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-console.log('Hello world!');
+
+const NORTH = 'North'
+const SOUTH = 'South'
+const EAST = 'East'
+const WEST = 'West'
+
+const go = (direction) => {
+    console.log(`Going ${direction}`)
+
+    const consoleEl = document.getElementById('console')
+    const consoleOutputEl = document.createElement('p')
+
+    consoleOutputEl.innerText = `Going ${direction}`
+
+    consoleEl.appendChild(consoleOutputEl)
+    consoleEl.scrollTop = consoleEl.scrollHeight;
+}
+
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'KeyW': go(NORTH)
+        break;
+        case 'KeyS': go(SOUTH)
+        break;
+        case 'KeyD': go(EAST)
+        break;
+        case 'KeyA': go(WEST)
+        break;
+    }
+})
