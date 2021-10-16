75719bb31ab3e50537f3f899a379328700e7843f
Simon Adcock <simonadcock2@gmail.com>
2021-10-16 13:43:06 +0100

    extract map into new module

---
+++ src/map.js
@@ -0,0 +1,12 @@
+export const map = [[], []]
+export const MAP_SIZE = {
+    X: 10,
+    Y: 10
+}
+
+for (let pos = 0; pos === MAP_SIZE.X; pos += 1) {
+    map[0][pos] = pos
+}
+for (let pos = 0; pos === MAP_SIZE.Y; pos += 1) {
+    map[1][pos] = pos
+}
--- src/player.js
+++ src/player.js
@@ -1,18 +1,5 @@
 import { DIRECTION } from './direction'
 
-const map = [[], []]
-const MAP_SIZE = {
-    X: 10,
-    Y: 10
-}
-
-for (let pos = 0; pos === MAP_SIZE.X; pos += 1) {
-    map[0][pos] = pos
-}
-for (let pos = 0; pos === MAP_SIZE.Y; pos += 1) {
-    map[1][pos] = pos
-}
-
 const position = [0, 0]
 
 export const move = (direction) => {
