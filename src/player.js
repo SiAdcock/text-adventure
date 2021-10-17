91a1ac6ee233efee7b5d6020e0a4b0fc794a705d
SiAdcock <simonadcock2@gmail.com>
2021-10-17 22:29:35 +0100

    add boundary checking function

--- src/player.js
+++ src/player.js
@@ -1,20 +1,38 @@
-import { DIRECTION } from './direction'
+import { DIRECTION } from "./direction";
+import { MAP_SIZE } from "./map";
 
-const position = [0, 0]
+const position = [0, 0];
+
+export const isNotOutOfBounds = (pos, direction) => {
+  switch (direction) {
+    case DIRECTION.NORTH:
+      return pos[1] + 1 < MAP_SIZE.Y;
+    case DIRECTION.SOUTH:
+      return pos[1] - 1 >= 0;
+    case DIRECTION.EAST:
+      return pos[0] + 1 < MAP_SIZE.X;
+    case DIRECTION.WEST:
+      return pos[0] - 1 >= 0;
+  }
+};
 
 export const move = (direction) => {
-    switch (direction) {
-        case DIRECTION.NORTH: position[1] = position[1] + 1
-        break;
-        case DIRECTION.SOUTH: position[1] = position[1] - 1
-        break;
-        case DIRECTION.EAST: position[0] = position[0] + 1
-        break;
-        case DIRECTION.WEST: position[0] = position[0] - 1
-        break;
-    }
-}
+  switch (direction) {
+    case DIRECTION.NORTH:
+      position[1] = position[1] + 1;
+      break;
+    case DIRECTION.SOUTH:
+      position[1] = position[1] - 1;
+      break;
+    case DIRECTION.EAST:
+      position[0] = position[0] + 1;
+      break;
+    case DIRECTION.WEST:
+      position[0] = position[0] - 1;
+      break;
+  }
+};
 
 export const getPosition = () => {
-    return position;
-}
+  return position;
+};
